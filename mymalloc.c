1bfc62cbd150234babd54475f7e1244595da622b
toogoodbruh <martialartsapplemma@gmail.com>
2023-02-22 17:13:28 -0500

    linked list struct for meta data

--- mymalloc.c
+++ mymalloc.c
@@ -9,13 +9,18 @@ static char memory[MEMSIZE];
 void *mymalloc(size_t size, char *file, int line){
     size_t *pMem = (size_t *)memory; //use pMem to write numbers in memory
     size_t headerSize = sizeof(size_t) + sizeof(char); //header includes size of chunk (short) and allocation status (char)
-    
+    struct metadata { //linked list to keep track of chunks
+        int free;
+        int size_t;
+        struct metadata *next;
+    };
+
     //initialize first chunk if not yet initialized
     if(pMem[0]==0){
         pMem[0] = MEMSIZE-headerSize; //size of first chunk is whole array minus header size
     }
     
-    printf("%d",pMem[0]); //test
+    printf("%ld",pMem[0]); //test
 }
 
 void myfree(void *ptr, char *file, int line){
