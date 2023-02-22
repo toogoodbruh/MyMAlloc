619e488d95b468efa48a557b93a77496b82f5d6a
Maylin Horchler <31592147+mahorchler@users.noreply.github.com>
2023-02-22 12:40:10 -0500

    Create test file

--- mymalloc.c
+++ mymalloc.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include "mymalloc.h"
 
-#define MEM_LENGTH 4096
+#define MEM_LENGTH  4096
+
 static char memory[MEM_LENGTH];
+
+void *mymalloc(size_t size, char *file, int line){
+    if(memory[0]==0){
+        //initialize chunk spanning entire array
+    }
+    printf("%d", memory[0]); //test initialization
+}
+
+void myfree(void *ptr, char *file, int line){
+
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymalloc.h"
+
+int main(int argc, char **argv)
+{
+    malloc(100);
+    return EXIT_SUCCESS;
+}
