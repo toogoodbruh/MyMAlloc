7b6793362b22ffc1dadfbabe864a7bf9d209480f
Maylin Horchler <31592147+mahorchler@users.noreply.github.com>
2023-02-22 15:42:35 -0500

    Mem First Write
    Wrote the first integer into memory (chunk size)

--- mymalloc.c
+++ mymalloc.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include "mymalloc.h"
 
-#define MEM_LENGTH  4096
+#define MEMSIZE  4096
 
-static char memory[MEM_LENGTH];
+static char memory[MEMSIZE];
 
 void *mymalloc(size_t size, char *file, int line){
-    if(memory[0]==0){
-        //initialize chunk spanning entire array
+    int *p = (int *)memory; //use p to write in memory with integers
+    if(p[0]==0){
+        p[0] = MEMSIZE-sizeof(int *)-1; //size of first chunk is whole array minus header size
+        //figure out how to write 0 and 1 (not allocated vs allocated) here
     }
-    printf("%d", memory[0]); //test initialization
+    printf("%d\n",p[0]); //test initialization
 }
 
 void myfree(void *ptr, char *file, int line){
